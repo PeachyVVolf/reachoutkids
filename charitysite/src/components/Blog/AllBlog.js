2c049c47042095bab6692a1065b58609734d4b58
Ahmad Ajmal <ahmadajmal9301@gmail.com>
2022-05-26 20:56:35 +0500

    bug tests

--- charitysite/src/components/Blog/AllBlog.js
+++ charitysite/src/components/Blog/AllBlog.js
@@ -11,7 +11,7 @@ const AllBlog = () => {
   
     useEffect(()=>{
       const fetchPosts = async () => {
-          const res = await axios.get("https://reachoutkids.herokuapp.com/post");
+          const res = await axios.get("/post");
           console.log(res.data);
           setPosts(res.data);
           console.log(posts);
